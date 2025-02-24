b2bd163a52e2d7dc6a19923f4cb4549dfbeda6ee
tryexceptend <try-except-end@gmail.com>
2025-02-24 20:18:45 +0300

    add finish

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -4,63 +4,38 @@
 # 1. Инициализация сервера конфигурации
 ###
 
-docker exec -it mongoConfigSrv mongosh --port 27017 <<EOF
-rs.initiate(
-  {
-    _id : "config_server",
-       configsvr: true,
-    members: [
-      { _id : 0, host : "mongoConfigSrv:27017" }
-    ]
-  }
-);
-exit();
+docker compose exec -T mongo_config mongosh --port 27017 <<EOF
+rs.initiate({_id : "config_server",configsvr: true, members: [{ _id : 0, host : "mongo_config:27017" }]})
 EOF
 
 ###
 # 2. Инициализация шарды 1
 ###
 
-docker exec -it mongoShard1 mongosh --port 27018 <<EOF
-rs.initiate(
-    {
-      _id : "shard1",
-      members: [
-        { _id : 0, host : "mongoShard1:27018" }
-      ]
-    }
-);
-exit();
+docker compose exec -T mongo_shard1 mongosh --port 27018 <<EOF
+rs.initiate({_id : "shard1",members: [{ _id : 0, host : "mongo_shard1:27018" }]})
 EOF
 
 ###
 # 3. Инициализация шарды 2
 ###
 
-docker exec -it mongoShard2 mongosh --port 27019 <<EOF
-rs.initiate(
-    {
-      _id : "shard1",
-      members: [
-        { _id : 1, host : "mongoShard2:27019" },
-      ]
-    }
-);
-exit();
+docker compose exec -T mongo_shard2 mongosh --port 27019 <<EOF
+rs.initiate({_id : "shard2",members: [{ _id : 1, host : "mongo_shard2:27019" }]})
 EOF
 
+sleep 10s
 ###
 # 4. Инициализация роутера
 ###
 
-docker exec -it mongosRouter mongosh --port 27020 <<EOF
-sh.addShard( "shard1/mongoShard1:27018");
-sh.addShard( "shard2/mongoShard1:27019");
-
-sh.enableSharding("somedb");
+docker compose exec -T mongo_router mongosh --port 27020 <<EOF
+sh.addShard( "shard1/mongo_shard1:27018")
+sh.addShard( "shard2/mongo_shard2:27019")
+sh.enableSharding("somedb")
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
-
 use somedb
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
 
+read -p "Press enter to continue"
--- sharding-repl-cache/scripts/mongo-init.sh
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -4,63 +4,42 @@
 # 1. Инициализация сервера конфигурации
 ###
 
-docker exec -it mongoConfigSrv mongosh --port 27017 <<EOF
-rs.initiate(
-  {
-    _id : "config_server",
-       configsvr: true,
-    members: [
-      { _id : 0, host : "mongoConfigSrv:27017" }
-    ]
-  }
-);
-exit();
+docker compose exec -T mongo_config mongosh --port 27017 <<EOF
+rs.initiate({_id : "config_server",configsvr: true, members: [{ _id : 0, host : "mongo_config:27017" }]})
 EOF
 
 ###
 # 2. Инициализация шарды 1
 ###
 
-docker exec -it mongoShard1 mongosh --port 27018 <<EOF
-rs.initiate(
-    {
-      _id : "shard1",
-      members: [
-        { _id : 0, host : "mongoShard1:27018" }
-      ]
-    }
-);
-exit();
+docker compose exec -T mongo_shard1-a mongosh --port 27018 <<EOF
+rs.initiate({_id: "shard1", members: [{_id: 0, host: "mongo_shard1-a:27018"},{_id: 1, host: "mongo_shard1-b:27019"},{_id: 2, host: "mongo_shard1-c:27020"}]})
 EOF
 
 ###
 # 3. Инициализация шарды 2
 ###
 
-docker exec -it mongoShard2 mongosh --port 27019 <<EOF
-rs.initiate(
-    {
-      _id : "shard1",
-      members: [
-        { _id : 1, host : "mongoShard2:27019" },
-      ]
-    }
-);
-exit();
+docker compose exec -T mongo_shard2-a mongosh --port 27021 <<EOF
+rs.initiate({_id: "shard2", members: [{_id: 0, host: "mongo_shard2-a:27021"},{_id: 1, host: "mongo_shard2-b:27022"},{_id: 2, host: "mongo_shard2-c:27023"}]})
 EOF
 
+sleep 10s
 ###
 # 4. Инициализация роутера
 ###
 
-docker exec -it mongosRouter mongosh --port 27020 <<EOF
-sh.addShard( "shard1/mongoShard1:27018");
-sh.addShard( "shard2/mongoShard1:27019");
-
-sh.enableSharding("somedb");
+docker compose exec -T mongo_router mongosh --port 27024 <<EOF
+sh.addShard( "shard1/mongo_shard1-a:27018")
+sh.addShard( "shard1/mongo_shard1-b:27019")
+sh.addShard( "shard1/mongo_shard1-c:27020")
+sh.addShard( "shard2/mongo_shard2-a:27021")
+sh.addShard( "shard2/mongo_shard2-b:27022")
+sh.addShard( "shard2/mongo_shard2-c:27023")
+sh.enableSharding("somedb")
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
-
 use somedb
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
 
+read -p "Press enter to continue"
